eaec08fd11087d1e4262c0c7bd635c88ba9ed642
Chintan Doshi <chintan_4190@yahoo.co.in>
2016-10-24 23:19:15 +0200

    no message

--- skcd/SKCDWeb/src/main/webapp/resources/theme1/js/product/global.js
+++
@@ -1,9 +0,0 @@
-/**
- *This contains global function which can be used across the site. 
- */
-function openAccordion(id){
-	$("#"+id).addClass("in");
-}
-function collapseAccordion(id){
-	$("#"+id).removeClass("in");
-}
